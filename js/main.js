3c135bf01aa59ad56f4ff61c2a7b1728e8cd5dba
Andrey <andrewart@yandex.ru>
2014-12-25 17:12:45 +0200

    a5

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+/*-- Popup --*/
+$('.btn_js').click(function () {
+	$('.overlay').fadeIn(300);
+	$('#pop1').fadeIn(300);
+});
+$('.callback').click(function () {
+	$('.overlay').fadeIn(300);
+	$('#pop2').fadeIn(300);
+});
+$('.overlay').click(function(){
+	$('.popup, .overlay').fadeOut(300);
+});
+/*-- End of Popup --*/
 
+$(".explain").hide();
+$( ".icon" ).hover(function() {
+  $(this).siblings(".explain, .tx").toggle();
+});
